9d3c2b4414f5755328b869587fe24c634e9f696f
bwierzbo <39303255+bwierzbo@users.noreply.github.com>
2023-03-17 11:18:47 -0400

    altered train.py to match ipynb

--- train.py
+++ train.py
@@ -15,9 +15,11 @@ import matplotlib.pyplot as plt
 
 import random
 import os
-print(os.listdir("../VolleyballTracking/"))
+import random
+import os
 
-filenames = os.listdir("../VolleyballTracking/coloroutpath/")
+
+filenames = os.listdir("../volleyball-tracking/data/coloroutpath/")
 categories = []
 
 for filename in filenames:
@@ -32,4 +34,107 @@ df = pd.DataFrame({
     'category': categories
 })
 
-print(df.head())
+FAST_RUN = False
+IMAGE_WIDTH=32
+IMAGE_HEIGHT=32
+IMAGE_SIZE=(IMAGE_WIDTH, IMAGE_HEIGHT)
+IMAGE_CHANNELS=3
+
+from keras.models import Sequential, Model
+from keras.layers import Conv2D
+from keras.layers import Input
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+from keras.layers import Activation
+from keras.layers import BatchNormalization
+from keras.layers import Dropout
+from keras.optimizers import RMSprop
+from keras.optimizers import SGD
+from keras.optimizers import Adam    
+
+model = Sequential()
+
+
+model.add(Conv2D(32,(3,3), activation='relu', input_shape=(IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS)))
+model.add(MaxPooling2D())
+
+model.add(Conv2D(64,(3,3), activation='relu'))
+model.add(MaxPooling2D())
+
+model.add(Flatten())
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.1))
+model.add(Dense(2, activation='softmax'))
+
+opt = SGD(lr=0.01)
+model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau
+
+earlystop = EarlyStopping(patience=10)
+
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_accuracy', 
+                                            patience=2, 
+                                            verbose=1, 
+                                            factor=0.5, 
+                                            min_lr=0.00001)
+
+callbacks = [earlystop, learning_rate_reduction]
+
+df["category"] = df["category"].replace({0: 'ball', 1: 'notball'}) 
+
+train_df, validate_df = train_test_split(df, test_size=0.20, random_state=42)
+train_df = train_df.reset_index(drop=True)
+validate_df = validate_df.reset_index(drop=True)
+
+total_train = train_df.shape[0]
+total_validate = validate_df.shape[0]
+batch_size=15
+
+train_datagen = ImageDataGenerator(
+    rotation_range=15,
+    rescale=1./255,
+    shear_range=0.1,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    width_shift_range=0.1,
+    height_shift_range=0.1
+)
+
+train_generator = train_datagen.flow_from_dataframe(
+    train_df, 
+    "../volleyball-tracking/data/coloroutpath/", 
+    x_col='filename',
+    y_col='category',
+    target_size=IMAGE_SIZE,
+    class_mode='categorical',
+    batch_size=batch_size
+)
+
+validation_datagen = ImageDataGenerator(rescale=1./255)
+validation_generator = validation_datagen.flow_from_dataframe(
+    validate_df, 
+    "../volleyball-tracking/data/coloroutpath/", 
+    x_col='filename',
+    y_col='category',
+    target_size=IMAGE_SIZE,
+    class_mode='categorical',
+    batch_size=batch_size
+)
+
+epochs=3 if FAST_RUN else 50
+history = model.fit_generator(
+    train_generator, 
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=total_validate//batch_size,
+    steps_per_epoch=total_train//batch_size,
+    callbacks=callbacks
+)
+
+model_json = model.to_json()
+with open("./model.json","w") as json_file:
+  json_file.write(model_json)
+
+model.save_weights("model.h5")
